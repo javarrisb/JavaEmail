29eb21f5e258208a8739699d78a52f35948f1b03
Javarrisb <javarrisb@gmail.com>
2023-03-11 10:14:50 -0500

    change springboot

--- src/main/java/com/app/email/Email.java
+++ src/main/java/com/app/email/Email.java
@@ -15,6 +15,12 @@ public class Email {
 
      // Constructor to receive the first name and last name
 
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email created:" + this.firstName + "" + this.lastName);
+    }
+
     // Ask for the department
 
     // Generate a random password
